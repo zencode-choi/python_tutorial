7bee7770ddd68897c12c15cc83200f9d342407cc
JWChoi <jwchoi1977@hotmail.com>
2026-05-26 22:50:41 +0900

    Update SKILL, pyproject.toml, requirements; add 03_advanced_use.py

--- 03_advanced_use.py
+++ 03_advanced_use.py
@@ -1,21 +1,5 @@
 from datetime import datetime
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from typing import List, Optional
 from pydantic import BaseModel, Field, EmailStr, ValidationError, field_validator
 
